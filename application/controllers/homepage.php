04d32aad4c646f3dcf95fb29bf2cf9497713f87c
Maisarohhasibuan <maisarohhasibuan@gmail.com>
2015-12-29 17:58:17 +0700

    homepage

--- application/controllers/homepage.php
+++ application/controllers/homepage.php
@@ -7,6 +7,7 @@ class Homepage extends CI_Controller {
 	$this->load->model('home_data');
 	$data['master_biota'] = $this->home_data->biota();
         //print_r($data);exit;
+		//test
         $this->load->view('homepage', $data);
         	
 }
